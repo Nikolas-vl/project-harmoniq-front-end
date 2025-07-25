4ea6c006fcf21658098875033dd861047ad5f632
Filbertik <aton2019zvit@gmail.com>
2025-07-25 12:26:05 +0300

    task/7_register_css

--- src/pages/UploadPhoto/UploadPhoto.jsx
+++ src/pages/UploadPhoto/UploadPhoto.jsx
@@ -1,5 +1,12 @@
-const UploadPhoto = () => {
-  return <div>UploadPhoto</div>;
+// const UploadPhoto = () => {
+//   return <div>UploadPhoto</div>;
+// };
+
+// export default UploadPhoto;
+
+// Додав заглушку
+const UploadPhotoPage = () => {
+  return <h1>Upload Photo Page (Coming soon...)</h1>;
 };
 
-export default UploadPhoto;
+export default UploadPhotoPage;
