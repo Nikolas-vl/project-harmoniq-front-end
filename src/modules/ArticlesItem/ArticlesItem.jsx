f8beda80b0faeedca0876ab436be452baaabb997
Oleksii <o.pruzhanskiy@gmail.com>
2025-07-25 16:26:33 +0300

    create articles list; implement modal window

---
+++ src/modules/ArticlesItem/ArticlesItem.jsx
@@ -0,0 +1,26 @@
+import ButtonAddToBookmarks from '../ButtonAddToBookmarks/ButtonAddToBookmarks';
+import s from './ArticlesItem.module.css';
+const ArticlesItem = ({ author, title, description, handleAdd }) => {
+  return (
+    <article className={s.articleContainer}>
+      <img
+        src="test"
+        alt="photo here"
+        width={337}
+        height={233}
+        className={s.articleImg}
+      />
+      <div className={s.textWrap}>
+        <h3 className={s.authorInfo}>{author}</h3>
+        <h3 className={s.titleInfo}>{title}</h3>
+        <p className={s.description}>{description}</p>
+      </div>
+      <div className={s.buttonWrap}>
+        <button className={s.learnMoreBtn}>Learn more</button>
+        <ButtonAddToBookmarks onAdd={handleAdd} />
+      </div>
+    </article>
+  );
+};
+
+export default ArticlesItem;
--- src/pages/ArticlesPage/ArticlesPage.jsx
+++ src/pages/ArticlesPage/ArticlesPage.jsx
@@ -1,5 +1,54 @@
+import ArticlesList from '../../modules/ArticlesList/ArticlesList';
+import '../../assets/styles/container.css';
+import SectionTitle from '../../modules/SectionTitle/SectionTitle';
+import { useState } from 'react';
 const ArticlesPage = () => {
-  return <div>ArticlesPage</div>;
+  const [filter, setFilter] = useState('All');
+  return (
+    <div className="container">
+      <SectionTitle>Articles</SectionTitle>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          marginBottom: '20px',
+          marginTop: '20px',
+          width: '100%',
+        }}
+      >
+        {/* // todo replace N to number of articles from back */}
+        <p
+          style={{
+            fontWeight: 700,
+            fontSize: '18px',
+            color: 'var(--color--black)',
+          }}
+        >
+          N articles
+        </p>
+        <select
+          value={filter}
+          name="select"
+          onChange={e => setFilter(e.target.value)}
+          style={{
+            width: '169px',
+            height: '33px',
+            padding: '4px 8px',
+            fontSize: '16px',
+            border: '1px solid #9F9F9F',
+            borderRadius: '4px',
+            cursor: 'pointer',
+          }}
+        >
+          <option value="All">All</option>
+          <option value="Popular">Popular</option>
+        </select>
+      </div>
+      <ArticlesList />
+      {/* // todo pagination */}
+    </div>
+  );
 };
 
 export default ArticlesPage;
