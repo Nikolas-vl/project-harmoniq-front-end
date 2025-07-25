051238eafb84e05bb29fc33f1367ae2c10fe0c6c
Eugen Alekseev <devilsbrother1@gmail.com>
2025-07-26 00:43:16 +0300

    Implement Create Article Page with form validation and image upload functionality

---
+++ src/modules/CreateArticleForm/components/InputWithScroll.jsx
@@ -0,0 +1,38 @@
+import { useRef } from 'react';
+import s from '../styles/index.module.css';
+
+export const InputWithScroll = ({ touched, error, value, onChange }) => {
+  const inputRef = useRef(null);
+
+  const handleFocus = () => {
+    const input = inputRef.current;
+    requestAnimationFrame(() => {
+      input.selectionStart = input.selectionEnd = input.value.length;
+      input.scrollLeft = input.scrollWidth;
+    });
+  };
+
+  const handleBlur = () => {
+    const input = inputRef.current;
+    input.scrollLeft = 0;
+  };
+
+  return (
+    <label className={s.titleLabel} htmlFor="titleInput">
+      Article Title
+      <input
+        name="title"
+        ref={inputRef}
+        type="text"
+        id="titleInput"
+        className={`${s.input} ${touched && error ? s.errored : ''}`}
+        placeholder="Enter the title"
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        value={value}
+        onChange={e => onChange(e.target.value)}
+      />
+      {touched && error && <span className={s.error}>{error}</span>}
+    </label>
+  );
+};
