d53270724d0d3f9f3365254a7fd38d88836d487d
ValkinDED <sonyerik289@gmail.com>
2025-07-30 18:47:44 +0200

    authOperations.js

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -1,11 +1,14 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios, { setAuthHeader, clearAuthHeader } from '../../api/axios';
 import { toast } from 'react-hot-toast';
+import { startLoading, stopLoading } from '../globalSlice';
 
 export const login = createAsyncThunk(
   'auth/login',
   async (credentials, thunkApi) => {
+    const { dispatch } = thunkApi;
     try {
+      dispatch(startLoading());
       const response = await axios.post('/auth/login', credentials);
       const { accessToken, user, userArticles, savedArticles } =
         response.data.data;
@@ -15,6 +18,8 @@ export const login = createAsyncThunk(
     } catch (e) {
       toast.error('Login error!');
       return thunkApi.rejectWithValue(e.message);
+    } finally {
+      dispatch(stopLoading());
     }
   }
 );
@@ -22,7 +27,9 @@ export const login = createAsyncThunk(
 export const register = createAsyncThunk(
   'auth/register',
   async (credentials, thunkApi) => {
+    const { dispatch } = thunkApi;
     try {
+      dispatch(startLoading());
       await axios.post('/auth/register', credentials);
       const { email, password } = credentials;
       const loginResult = await thunkApi.dispatch(login({ email, password }));
@@ -40,25 +47,33 @@ export const register = createAsyncThunk(
       }
       toast.error('Server error!');
       return thunkApi.rejectWithValue({ general: 'Server error' });
+    } finally {
+      dispatch(stopLoading());
     }
   }
 );
 
 export const logout = createAsyncThunk('auth/logout', async (_, thunkApi) => {
+  const { dispatch } = thunkApi;
   try {
+    dispatch(startLoading());
     await axios.post('/auth/logout');
     clearAuthHeader();
     toast.success('Logged out successfully!');
   } catch (e) {
     toast.error('Logout error!');
     return thunkApi.rejectWithValue(e.message);
+  } finally {
+    dispatch(stopLoading());
   }
 });
 
 export const refreshUser = createAsyncThunk(
   'auth/refresh',
   async (_, thunkAPI) => {
+    const { dispatch } = thunkAPI;
     try {
+      dispatch(startLoading());
       const response = await axios.post('/auth/refresh');
       const { accessToken, user, userArticles, savedArticles } =
         response.data.data;
@@ -66,6 +81,8 @@ export const refreshUser = createAsyncThunk(
       return { accessToken, user, userArticles, savedArticles };
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
+    } finally {
+      dispatch(stopLoading());
     }
   }
 );
