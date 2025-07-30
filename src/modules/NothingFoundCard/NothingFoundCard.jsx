6bcb19fa657281eb6d7971ee297e17e47460b791
Bogdan_141 <bogdanmakovey1001@gmail.com>
2025-07-30 22:19:29 +0200

    finish NothingFoundCard, minor changes in PopularArticleCard

--- src/modules/NothingFoundCard/NothingFoundCard.jsx
+++ src/modules/NothingFoundCard/NothingFoundCard.jsx
@@ -1,13 +1,17 @@
 import css from './NothingFoundCard.module.css';
 import logo from '/src/assets/icons/alert.svg';
+import { Link } from 'react-router-dom';
 
-const NothingFoundCard = () => {
+const NothingFoundCard = ({ title, text, linkText, linkPath }) => {
     return (
         <>
             <div className={css.main_container}>
+                <div>
                     <img src={logo} alt="Exclamation mark in the centre of circle" />
-                    <h4 className={css.not_found_title}>Nothing found.</h4>
-                    <span className={css.not_found_text}>Be the first, who create an article</span>
+                    <h4 className={css.not_found_title}>{title}</h4>
+                    <span className={css.not_found_text}>{text}</span>
+                </div>
+                <Link to={linkPath} className={css.redirect_link}>{linkText}</Link>
             </div>
         </>
     );
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,22 +2,14 @@ import PopularArticles from '../../modules/PopularArticles/PopularArticles';
 import Creators from '../../modules/Creators/Creators';
 import Hero from '../../modules/Hero/Hero';
 import About from '../../modules/About/About';
-import NothingFoundCard from '../../modules/NothingFoundCard/NothingFoundCard';
 
 const HomePage = () => {
   return (
     <>
-      <div style={{
-        display: 'flex',
-        alignItems: 'center',
-        flexDirection: 'column'
-      }}>
       <Hero />
       <About />
       <PopularArticles />
       <Creators />
-      <NothingFoundCard />
-      </div>
     </>
   );
 };
