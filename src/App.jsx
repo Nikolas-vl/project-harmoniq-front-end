f63a1f6bb272514ec2e6d921248a3b433ae000df
ValkinDED <sonyerik289@gmail.com>
2025-07-30 16:23:12 +0200

    task/41-Loader-finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Toaster } from 'react-hot-toast';
 import Layout from './modules/Layout/Layout';
 import AppRoutes from './routes/AppRoutes';
-import { useDispatch, useSelector } from 'react-redux';
 import { refreshUser } from './redux/auth/authOperations';
-import { useEffect } from 'react';
-// import { selectUserToken } from './redux/auth/authSelectors';
 import Loader from './modules/Loader/Loader';
 
 function App() {
   const dispatch = useDispatch();
-  const isLoading = useSelector(state => state.global.isLoading);
 
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
 
-  if (isLoading) {
-    return <Loader fullScreen size={50} color="#4fa94d" />;
-  }
-
   return (
     <>
       <Toaster position="top-center" />
+      <Loader /> {}
       <Layout>
         <AppRoutes />
       </Layout>
