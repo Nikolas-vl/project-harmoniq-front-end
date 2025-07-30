484427bc53662b155bde78595085acf544c6a6ec
Oleksii <o.pruzhanskiy@gmail.com>
2025-07-30 16:00:49 +0300

    fix add article to favorites

--- src/modules/ArticlesList/ArticlesList.jsx
+++ src/modules/ArticlesList/ArticlesList.jsx
@@ -10,21 +10,39 @@ import {
   selectUserId,
 } from '../../redux/auth/authSelectors';
 import { useSaveArticle } from '../../api/hooks/users/useSaveArticle';
+import toast from 'react-hot-toast';
 
 const ArticlesList = ({ articles }) => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
   const userId = useSelector(selectUserId);
   const savedArticles = useSelector(selectSavedArticles);
-
+  const [savedIds, setSavedIds] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const { saveArticle, isLoading } = useSaveArticle();
+
+  useEffect(() => {
+    if (isLoggedIn && savedArticles) {
+      setSavedIds(savedArticles.map(article => article._id));
+    }
+  }, [isLoggedIn, savedArticles]);
 
   const handleAdd = async article_id => {
     if (!isLoggedIn) {
       setShowModal(true);
-    } else {
-      await saveArticle(userId, article_id);
       return;
+    }
+
+    if (savedIds.includes(article_id)) {
+      toast('Already in favourites!');
+      return;
+    }
+    try {
+      await saveArticle(userId, article_id);
+      setSavedIds(prev => [...prev, article_id]);
+      toast.success('Saved!');
+    } catch (error) {
+      console.error('Failed to save:', error);
+      toast.error('Something went wrong');
     }
   };
 
@@ -40,10 +58,10 @@ const ArticlesList = ({ articles }) => {
               img={article.img}
               handleAdd={handleAdd}
               article_id={article._id}
-              isSaved={savedArticles.some(
-                articleInList => articleInList._id === article?._id
-              )}
-              // isSaved={allSavedArticles.includes(article._id)}
+              // isSaved={savedArticles.some(
+              //   articleInList => articleInList._id === article?._id
+              // )}
+              isSaved={savedIds.includes(article._id)}
             />
           </li>
         ))}
