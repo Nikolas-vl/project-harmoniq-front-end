298fae67b9ffa67ab8831f8e8e34aa37ad9fdc7b
Nikolas-vl <nikvv02@gmail.com>
2025-08-01 16:54:13 +0000

    Deploying to gh-pages from @ Nikolas-vl/project-harmoniq-front-end@c9e956bf78ff83e155d3774a44194a4ec7739efa 🚀

--- assets/useGetArticleById-C3CbCDKe.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index-BxbR7xre.js";import{a as l}from"./articlesApi-Bsa7safb.js";const u=t=>{const[s,c]=r.useState(null),[o,a]=r.useState(!1);return r.useEffect(()=>{if(!t)return;(async()=>{a(!0);try{const e=await l(t);c(e.data.data.article)}catch(e){console.error("Failed to fetch article:",e)}finally{a(!1)}})()},[t]),{article:s,isLoading:o}};export{u};
