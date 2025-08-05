1e303c74855dacf20e6f22431873b97a0980a5a5
Nikolas-vl <nikvv02@gmail.com>
2025-08-05 02:50:17 +0200

    unknown_avatar

--- src/modules/AuthorsList/AuthorsItem.jsx
+++ src/modules/AuthorsList/AuthorsItem.jsx
@@ -1,5 +1,6 @@
 import css from '../../pages/AuthorsPage/AuthorsPage.module.css';
 import { useNavigate } from 'react-router-dom';
+import photoPlaceholder from '../../assets/icons/uploadPhoto/photo.svg';
 
 const MAX_LENGTH = 18;
 
@@ -12,10 +13,16 @@ export const AuthorsItem = ({ id, name, avatar }) => {
 
   const trimmedName =
     name.length > MAX_LENGTH ? name.slice(0, MAX_LENGTH) + '...' : name;
-
+  const isAvatar = Boolean(avatar);
   return (
     <li className={css.authorItem} onClick={handleClick}>
-      <img src={avatar} alt={name} className={css.avatar} />
+      {isAvatar ? (
+        <img src={avatar} alt={name} className={css.avatar} />
+      ) : (
+        <div className={css.unknown_avatar}>
+          <img src={photoPlaceholder} alt="placeholder" />
+        </div>
+      )}
       <p className={css.name}>{trimmedName}</p>
     </li>
   );
