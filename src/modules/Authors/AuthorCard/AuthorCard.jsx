1d3f48a0be725f1c2befe3c4a4de46ce17ea956e
Eugen Alekseev <devilsbrother1@gmail.com>
2025-08-05 12:37:32 +0300

    fix popular author card

--- src/modules/Authors/AuthorCard/AuthorCard.jsx
+++ src/modules/Authors/AuthorCard/AuthorCard.jsx
@@ -1,8 +1,13 @@
 import css from './AuthorCard.module.css';
 import { Link } from 'react-router-dom';
 import Camera from '../../../assets/icons/createArticlePage/camera.svg?react';
+import { MAX_LENGTH } from '../../../constants/authorsItem';
 
 const AuthorCard = ({ users }) => {
+  const trimmedName =
+    users.name.length > MAX_LENGTH
+      ? users.name.slice(0, MAX_LENGTH) + '...'
+      : users.name;
   return (
     <Link to={`/authors/${users._id}`} className={css.card_container}>
       {users.avatarUrl ? (
@@ -14,7 +19,7 @@ const AuthorCard = ({ users }) => {
       ) : (
         <Camera className={css.unknown_image} />
       )}
-      <span className={css.author_name}>{users.name}</span>
+      <span className={css.author_name}>{trimmedName}</span>
     </Link>
   );
 };
