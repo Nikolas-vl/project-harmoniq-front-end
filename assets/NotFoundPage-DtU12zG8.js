5e0d814ac244350ad113394955f453af1b2d85a4
Nikolas-vl <nikvv02@gmail.com>
2025-08-05 00:35:35 +0000

    Deploying to gh-pages from @ Nikolas-vl/project-harmoniq-front-end@a81c52a5489ae6dd478ce86d28375a7c976fc09f 🚀

---
+++ assets/NotFoundPage-DtU12zG8.js
@@ -0,0 +1,1 @@
+import{j as t,N as o}from"./index-xz-e4_RC.js";const s=()=>t.jsx(o,{to:"/"});export{s as default};
--- assets/useSaveArticle-DgIpWPoy.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./index-FuH_kWGc.js";import{s as c}from"./usersApi-xHuqhiyv.js";const f=()=>{const[r,e]=o.useState(!1);return{saveArticle:async(s,t)=>{e(!0);try{await c(s,t)}catch(a){console.error("Failed to save article:",a)}finally{e(!1)}},isLoading:r}};export{f as u};
