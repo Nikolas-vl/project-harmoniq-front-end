84923e16e7bd6f4993d631ad932511644279babb
Nikolas-vl <nikvv02@gmail.com>
2025-08-07 12:43:56 +0000

    Deploying to gh-pages from @ Nikolas-vl/project-harmoniq-front-end@b05e1ff5b4bdf2141161a55f2df471184f1a718d 🚀

--- assets/NotFoundPage-Bm1XjxNP.js
+++
@@ -1,1 +0,0 @@
-import{j as t,N as o}from"./index-Br6ym3h2.js";const s=()=>t.jsx(o,{to:"/"});export{s as default};
---
+++ assets/NotFoundPage-BpyUTZf-.js
@@ -0,0 +1,1 @@
+import{j as t,N as o}from"./index-BfNYoVeA.js";const s=()=>t.jsx(o,{to:"/"});export{s as default};
